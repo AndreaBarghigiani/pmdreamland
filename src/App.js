4568ba09b133c4644c7e63838318753e398fe407
Andrea Barghigiani <andrea.barghigiani@incsub.com>
2021-01-10 07:40:37 +0100

    minor refactor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import {
-	Box,
+	Container,
 	VStack,
 	Grid,
 	Heading
 } from '@chakra-ui/react';
 
-import Header from './layout/Header'
 import Entry from './components/Entry';
 import { useUser } from './lib/UserContext'
 
 function App() {
 	const [user] = useUser(); 
-	const [ addTask, setAddTask ] = React.useState(false)
 	return (
-		<Box textAlign="center" fontSize="xl">
-			<Grid p={3}>
-				<Header addTask={setAddTask} />
+		<Container maxW='xl' p={3} centerContent fontSize="xl">
 				{user ? (
 					<VStack spacing={8}>
-						<Entry showForm={addTask} />
+						<Entry />
 					</VStack>
 				) : (
 					<Heading>You must be logged in</Heading>
 				)}
-			</Grid>
-		</Box>
+		</Container>
 	);
 }
 
