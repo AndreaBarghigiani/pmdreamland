f773b5e5bffa4880da9a49e6ae89c1f9dfe485bd
Andrea Barghigiani <andrea.barghigiani@incsub.com>
2021-01-20 08:12:45 +0100

    first iteration, copy working need to include more info

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -6,7 +6,10 @@ import {
 	Spacer,
 	Tooltip,
 	IconButton,
+	Divider,
+	Button,
 	useDisclosure,
+	useToast,
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 import SingleTask from './SingleTask';
@@ -15,11 +18,37 @@ import TaskModal from './TaskModal';
 import { fetchUserTasks } from '../lib/Store';
 import { useUser } from '../lib/UserContext';
 
+function transformTaskToString(index, { name, progress }) {
+	const taskString = `
+*Task ${++index}*
+_Title_: ${name}
+_Progress_: ${progress}
+----------------------------------------`;
+	return taskString;
+}
+
+/**
+ * Simple function that let copy text from any field.
+ *
+ * @param {Object} item The element where we need to take text from.
+ *
+ * @return {boolean} If text already copied.
+ */
+function copyToClipboard( item ) {
+	const fakeInput = document.createElement( 'textarea' );
+	fakeInput.value = item;
+	document.body.appendChild( fakeInput );
+	fakeInput.select();
+	document.execCommand( 'copy' );
+	document.body.removeChild( fakeInput );
+};
+
 const TasksList = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [tasks, setTasks] = React.useState([]);
 	const [task, setTask] = React.useState(null);
 	const [user] = useUser();
+	const toast = useToast();
 
 	// Fetching user tasks
 	React.useEffect(() => fetchUserTasks(user.id).then(res => setTasks(res)), [
@@ -33,6 +62,20 @@ const TasksList = () => {
 		setTask(task);
 		onOpen();
 	};
+
+	const copyForSlack = () => {
+		let tmpMarkDown = tasks.map((task, i) => transformTaskToString(i, task));
+		copyToClipboard(tmpMarkDown.join(''));
+		toast({
+			position: 'bottom-right',
+			title: 'Tasks Copied',
+			description: "You're can copy your report into Slack now",
+			status: 'success',
+			duration: 5000,
+			isClosable: true,
+		});
+	};
+
 	return (
 		<>
 			{tasks?.length > 0 ? (
@@ -57,6 +100,12 @@ const TasksList = () => {
 					{tasks.map(task => (
 						<SingleTask key={task.id} task={task} openModal={openModal} />
 					))}
+					<Divider />
+					<Flex justify={'flex-end'}>
+						<Button onClick={copyForSlack} colorScheme={'blue'}>
+							Copy for Slack
+						</Button>
+					</Flex>
 					<TaskModal isOpen={isOpen} onClose={onClose} task={task} />
 				</Stack>
 			) : (
